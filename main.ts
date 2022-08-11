c8069310a57ca934a5dde86199be52e1d3edce79
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-12 00:02:06 +0900

    root

--- main.ts
+++ main.ts
@@ -1,2 +1,5 @@
 const p = new URLPattern({ pathname: "/:text.png" });
-console.log(p.exec("http://a.com/aaa.png"));
+console.log(p.exec("http://a.com/👪.png")?.pathname.groups.text);
+console.log(p.exec("http://a.com/.png")?.pathname.groups.text);
+console.log(p.exec("http://a.com/a/a.png")?.pathname.groups.text);
+console.log(p.exec("http://a.com/a.png")?.pathname.groups.text);
--- serve.ts
+++ serve.ts
@@ -3,9 +3,14 @@ import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
 import { getEmojiSVG, isEmoji } from "./src/twemoji.ts";
 import { getTextSVG, svgToPng } from "./src/text.ts";
 
+const PNG_PATTERN = new URLPattern({ pathname: "/:text.png" });
+
 serve(async (req) => {
-  const { searchParams } = new URL(req.url);
-  const [str] = searchParams.get("char") ?? "🦕";
-  const svg = isEmoji(str) ? await getEmojiSVG(str) : getTextSVG(str);
-  return new Response(await svgToPng(svg));
+  const match = PNG_PATTERN.exec(req.url);
+  if (match) {
+    const [str] = decodeURI(match.pathname.groups.text);
+    const svg = isEmoji(str) ? await getEmojiSVG(str) : getTextSVG(str);
+    return new Response(await svgToPng(svg));
+  }
+  return new Response("200 OK");
 });
