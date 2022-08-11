da9a8a6778fab15ca6bf31d805378d876898d9bc
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-12 03:32:19 +0900

    update

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
+import "https://deno.land/std@0.152.0/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 await start(manifest);
--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,6 +1,6 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { createReporter } from "https://deno.land/x/g_a@0.1.2/mod.ts";
-const ga = createReporter({ id: "G-RD07HYF1TK" });
+const ga = createReporter({ id: Deno.env.get("ANALYTICS_UA") });
 
 export async function handler(
   req: Request,
@@ -13,6 +13,7 @@ export async function handler(
     return res;
   } catch (e) {
     err = e;
+    throw e;
   } finally {
     ga(req, ctx, res, start, err);
   }
