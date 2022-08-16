0995f5d286e6e2b618375b1e0cd21a617537f8bf
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-16 14:42:40 +0900

    add test

---
+++ utils/text_test.ts
@@ -0,0 +1,15 @@
+import { assertEquals } from "https://deno.land/std@0.152.0/testing/asserts.ts";
+
+import { getTextSVG, svgToPng } from "./text.ts";
+
+Deno.test({
+  name: "svgToPng",
+  async fn() {
+    const svg = getTextSVG("a", "red");
+    const png = await svgToPng(svg);
+    assertEquals(
+      png,
+      Deno.readFileSync(new URL(import.meta.resolve("./testdata/a.png"))),
+    );
+  },
+});
