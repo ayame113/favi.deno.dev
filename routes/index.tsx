f78ea72185d453e88e5619ba5d1c6cc7328a1bc6
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-20 21:56:15 +0900

    fix: twitter card display (#4)
    * feat: add meta tag
    
    * try twitter card validator
    
    * try twitter card 2

--- routes/index.tsx
+++ routes/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         <meta property="og:title" content={TITLE} />
         <meta property="og:description" content={DESCRIPTION} />
         <meta property="og:url" content="https://favi.deno.dev" />
-        <meta property="og:image" content={FAVICON_URL} />
+        <meta property="og:image" content="https://favi.deno.dev/🍌.png" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@_ayame113_" />
